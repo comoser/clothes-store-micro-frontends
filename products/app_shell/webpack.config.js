90b41c1ba31928534d0e9052f9b9e7f07cf5e12c
David Alecrim <d.alecrim@xgeeks.io>
2021-01-27 15:17:01 +0000

    feat(app_shell): init app shell MFE

---
+++ products/app_shell/webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const deps = require('./package.json').dependencies;
+
+module.exports = {
+  output: {
+    publicPath: 'http://localhost:3000/',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'app_shell',
+      // remotes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
